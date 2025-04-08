afbd48481644b8b68655d76e91198e790df378f5
MikeJollie2707 <bachngo@yahoo.com>
2025-04-07 21:46:34 -0700

    Add naive implementation of gradient LSE

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def LSE(pred, y):
-    return (y - pred) ** 2
+    return (pred - y) ** 2
 def gradient_LSE(betas, X_batch, y_batch):
-    # Loss function: (y - pred)^2
-    # Gradient w.r.t m and b: <2(y - pred) * d/dm(y - pred), 2(y - pred) * d/db(y - pred)>
-    # = <2(y - (mx_i + b)) * -x_i, 2(y - (mx_i + b)) * 1>
+    """Naively return the gradient of LSE loss function.
+    Return: np.array[[change in y-intercept], [change in slope]]
+    """
+    # Loss function: (pred_i - y_i)^2
+    # Gradient w.r.t b and m: <2(pred_i - y) * d/db(pred_i - y), 2(pred_i - y) * d/dm(pred_i - y)>
+    # = <2(mx_i + b - y_i) * 1, 2(mx_i + b - y_i) * x_i>
 
-    # y - (mx_i + b) = y_batch - (X_batch * betas) where X_batch is a nx2 matrix with first column all 1s and second col the actual x.
-    prediction_diff = (y_batch - (np.cross(X_batch, betas))) * 2
-    pass
+    gradients = np.zeros((2, 1))
+    b = betas[0][0]
+    m = betas[1][0]
+    for i in range(len(X_batch)):
+        x_i = X_batch[i][1] # The X_bias is [[1, x_i], ...] and we want x_i
+        y_i = y_batch[i][0]
+        prediction_diff = 2 * (m * x_i + b - y_i)
+        gradient = np.array([[prediction_diff], [prediction_diff * x_i]])
+        gradients += gradient
+    return gradients
+
+def gradient_LSE_lnalg(betas, X_batch, y_batch):
+    """Return the gradient of LSE loss function with style.
+    Return: np.array[[change in y-intercept], [change in slope]]
+    """
+    # https://www.stat.cmu.edu/~cshalizi/mreg/15/lectures/13/lecture-13.pdf
+    return 2 * X_batch.T.dot(X_batch.dot(betas) - y_batch)
 
 np.random.seed(42)
 # Vector of inputs
@@ -22,28 +39,40 @@ y = 4 + 3 * X + np.random.randn(100, 1)
 def sgd(X, y, learning_rate, epochs, batch_size):
     X_size = len(X)
     X_bias = np.c_[np.ones((X_size, 1)), X]
-    # [[m], [b]]
-    beta = np.random.randn(2, 1)
+    # [[b], [m]]
+    betas = np.random.randn(2, 1)
 
     cost_history = []
-    rng = np.random.default_rng()
 
     for epoch in range(epochs):
         # Move the rows only, keep the column the same.
-        indices = rng.permutation(X_size)
+        indices = np.random.permutation(X_size)
         X_shuffle = X_bias[indices]
         y_shuffle = y[indices]
 
-        # Instead of "true" SGD where we only calc for the loss function of one data point,
-        # we calc a few data points to make convergence smoother.
+        # For each epoch, we go through all points at least once.
+        for i in range(0, X_size, batch_size):
+            X_batch = X_shuffle[i:i + batch_size]
+            y_batch = y_shuffle[i:i + batch_size]
 
-        X_batch = X_shuffle[0:batch_size]
-        y_batch = y_shuffle[0:batch_size]
+            gradient = gradient_LSE(betas, X_batch, y_batch)
+            betas -= learning_rate * gradient / batch_size
 
-        beta -= learning_rate * gradient_LSE(beta, 1, y_batch)
+        # [1, x] * [[b], [m]] -> [b + x * m]
+        predictions = X_bias @ betas
+        cost = np.mean(LSE(predictions, y))
+        cost_history.append(cost)
 
+        if epoch % 100 == 0:
+            print(f"Epoch: {epoch}, cost: {cost}")
+            print(f"Beta:\n{betas}")
+            print("===================")
+    
+    print(f"Final: {betas}")
+
+    return betas, cost_history
 
 
 if __name__ == "__main__":
-    sgd(X, y, 0.01, 1, 2)
+    sgd(X, y, 0.1, 1000, 10)
 
